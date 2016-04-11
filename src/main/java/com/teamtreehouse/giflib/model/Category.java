ad4e887be24366c2e427a66611b62ecadd24aee6
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-04-11 14:07:05 -0500

    Added JPA annotations to entities

--- src/main/java/com/teamtreehouse/giflib/model/Category.java
+++ src/main/java/com/teamtreehouse/giflib/model/Category.java
@@ -1,12 +1,18 @@
 package com.teamtreehouse.giflib.model;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String colorCode;
+
+    @OneToMany(mappedBy = "category")
     private List<Gif> gifs = new ArrayList<>();
 
     public Category(){}
--- src/main/java/com/teamtreehouse/giflib/model/Gif.java
+++ src/main/java/com/teamtreehouse/giflib/model/Gif.java
@@ -1,12 +1,20 @@
 package com.teamtreehouse.giflib.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
+@Entity
 public class Gif {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Lob
     private byte[] bytes;
     private String description;
+
+    @ManyToOne
     private Category category;
     private LocalDateTime dateUploaded = LocalDateTime.now();
     private String username = "You";
