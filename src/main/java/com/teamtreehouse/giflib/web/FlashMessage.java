8fe1e033a88032c2b94f999e741af8bbef4631dd
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-04-12 12:03:06 -0500

    Added FlashMessage class

---
+++ src/main/java/com/teamtreehouse/giflib/web/FlashMessage.java
@@ -0,0 +1,23 @@
+package com.teamtreehouse.giflib.web;
+
+public class FlashMessage {
+    private String message;
+    private Status status;
+
+    public FlashMessage(String message, Status status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public static enum Status {
+        SUCCESS, FAILURE
+    }
+}
