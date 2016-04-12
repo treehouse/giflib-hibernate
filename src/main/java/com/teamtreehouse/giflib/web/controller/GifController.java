5eff791cfd6a4d8909d7d8cff8f36403172cc656
Chris Ramacciotti <chris.j.rama@gmail.com>
2016-04-12 15:12:17 -0500

    Implemented GIF upload in controller

--- src/main/java/com/teamtreehouse/giflib/web/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/web/controller/GifController.java
@@ -2,17 +2,23 @@ package com.teamtreehouse.giflib.web.controller;
 
 import com.teamtreehouse.giflib.model.Gif;
 import com.teamtreehouse.giflib.service.CategoryService;
+import com.teamtreehouse.giflib.service.GifService;
+import com.teamtreehouse.giflib.web.FlashMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class GifController {
+    @Autowired
+    private GifService gifService;
+
     @Autowired
     private CategoryService categoryService;
 
@@ -30,7 +36,7 @@ public class GifController {
     @RequestMapping("/gifs/{gifId}")
     public String gifDetails(@PathVariable Long gifId, Model model) {
         // TODO: Get gif whose id is gifId
-        Gif gif = null;
+        Gif gif = gifService.findById(gifId);
 
         model.addAttribute("gif", gif);
         return "gif/details";
@@ -41,7 +47,7 @@ public class GifController {
     @ResponseBody
     public byte[] gifImage(@PathVariable Long gifId) {
         // TODO: Return image data as byte array of the GIF whose id is gifId
-        return null;
+        return gifService.findById(gifId).getBytes();
     }
 
     // Favorites - index of all GIFs marked favorite
@@ -57,11 +63,15 @@ public class GifController {
 
     // Upload a new GIF
     @RequestMapping(value = "/gifs", method = RequestMethod.POST)
-    public String addGif(@RequestParam MultipartFile file) {
+    public String addGif(Gif gif, @RequestParam MultipartFile file, RedirectAttributes redirectAttributes) {
         // TODO: Upload new GIF if data is valid
+        gifService.save(gif,file);
+
+        // Add flash message for success
+        redirectAttributes.addFlashAttribute("flash",new FlashMessage("GIF successfully uploaded!", FlashMessage.Status.SUCCESS));
 
         // TODO: Redirect browser to new GIF's detail view
-        return null;
+        return String.format("redirect:/gifs/%s",gif.getId());
     }
 
     // Form for uploading a new GIF
